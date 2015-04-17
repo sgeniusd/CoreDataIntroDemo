167f66ce4df147bcc7f2caab569b3e64f3e45a05
songd <songd@yiche.com>
2015-04-17 10:56:27 +0800

    Initial Commit

---
+++ CoreDataIntroDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataIntroDemo
+//
+//  Created by bita on 15/4/17.
+//  Copyright (c) 2015年 bita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataIntroDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreDataIntroDemo
+//
+//  Created by bita on 15/4/17.
+//  Copyright (c) 2015年 bita. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
